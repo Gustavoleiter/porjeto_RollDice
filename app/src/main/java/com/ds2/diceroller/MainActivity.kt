7e4401cb0a92c150ce36f48da7b5c718518ade3e
gustavo <gustavu0720@gmail.com>
2023-03-22 20:14:41 -0300

    finalização do projeto

--- app/src/main/java/com/ds2/diceroller/MainActivity.kt
+++ app/src/main/java/com/ds2/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ds2.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,19 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton:  Button = findViewById(R.id.buttondice)
-        rollButton.setOnClickListener{
+        val rollButton: Button = findViewById(R.id.buttondice)
+        rollButton.setOnClickListener {
 
         }
         val toast = Toast.makeText(this, "Rolou um Dado!", Toast.LENGTH_SHORT).show()
-        val resultTextView: TextView= findViewById(R.id.textView)
         rollButton.setOnClickListener {
             rollDice()
         }
     }
-    class Dice(val numSides:Int){
-        fun roll():Int {
-            return (1..numSides ) . random()
+
+    class Dice(val numSides: Int) {
+        fun roll(): Int {
+            return (1..numSides).random()
         }
 
     }
@@ -30,12 +31,18 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+
     }
-
-
-
-
 }
-
